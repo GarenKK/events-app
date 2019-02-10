03115fb5636b61dfc2487a455e7fda6f087e56c3
Garen <GarenKK@gmail.com>
2019-02-10 23:36:49 +0200

    workaround for nano bugging in some environments

--- backend/database/database.js
+++ backend/database/database.js
@@ -7,6 +7,7 @@ var conf = require('../conf.js'),
     db,
     setCookie,
     authorize,
+    resetInterval,
     interval,
     database = {};
 
@@ -29,52 +30,48 @@ authorize = function () {
     });
 };
 
+resetInterval = function (headers) {
+    if (headers && headers['set-cookie']) {
+        clearInterval(interval);
+        setCookie(headers['set-cookie'][0]);
+        interval = setInterval(authorize, timeout);
+    }
+};
+
 database.view = function (designDoc, viewName, params) {
-    return db.view(designDoc, viewName, params, function (err, body, headers) {
-        if (headers && headers['set-cookie']) {
-            clearInterval(interval);
-            setCookie(headers['set-cookie'][0]);
-            interval = setInterval(authorize, timeout);
-        }
+    // nano bug doesnt allow db.view to return promise like db.get and db.insert while having callback in some environments
+    return new Promise(function (resolve, reject) {
+        db.view(designDoc, viewName, params, function (err, body, headers) {
+            resetInterval(headers);
+            if (err) {
+                reject(err);
+            } else {
+                resolve(body);
+            }
+        });
     });
 };
 
 database.get = function (docId) {
     return db.get(docId, function (err, body, headers) {
-        if (headers && headers['set-cookie']) {
-            clearInterval(interval);
-            setCookie(headers['set-cookie'][0]);
-            interval = setInterval(authorize, timeout);
-        }
+        resetInterval(headers);
     });
 };
 
 database.insert = function (body, id) {
     if (typeof id === 'string' || id instanceof String) {
         return db.insert(body, id, function (err, body, headers) {
-            if (headers && headers['set-cookie']) {
-                clearInterval(interval);
-                setCookie(headers['set-cookie'][0]);
-                interval = setInterval(authorize, timeout);
-            }
+            resetInterval(headers);
         });
     }
     return db.insert(body, function (err, body, headers) {
-        if (headers && headers['set-cookie']) {
-            clearInterval(interval);
-            setCookie(headers['set-cookie'][0]);
-            interval = setInterval(authorize, timeout);
-        }
+        resetInterval(headers);
     });
 };
 
 database.destroy = function (id, rev) {
     return db.destroy(id, rev, function (err, body, headers) {
-        if (headers && headers['set-cookie']) {
-            clearInterval(interval);
-            setCookie(headers['set-cookie'][0]);
-            interval = setInterval(authorize, timeout);
-        }
+        resetInterval(headers);
     });
 };
 
